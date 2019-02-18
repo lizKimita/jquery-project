751903987d6f1359df88d7044757dddb223f00a3
lizKimita <kimita.wanjiku@gmail.com>
2019-02-18 18:26:24 +0300

    adding practice examples

--- js/scripts.js
+++ js/scripts.js
@@ -3,4 +3,11 @@ $(document).ready(function() {
     $("#walrus-showing").toggle();
     $("#walrus-hidden").toggle();
   });
+
+  $("#fade1").click(function() {
+    $("#div1").slideToggle();
+  });
+  $("#fade2").click(function(){
+    $("#div1").slideToggle();
+  });
 });
